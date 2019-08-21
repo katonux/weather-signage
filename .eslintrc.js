e963e045db18a88b38da5aece2008cffdc360b49
Shota Kato <sheeptech@vivaldi.net>
2019-08-21 10:57:30 +0900

    eslintのルール消した

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,8 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     "plugin:vue/recommended",
-    "plugin:prettier/recommended"
+    "eslint:recommended",
+    //"plugin:prettier/recommended"
   ],
   // required to lint *.vue files
   plugins: [
@@ -19,9 +20,5 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    "semi": [2, "never"],
-    "no-console": "off",
-    "vue/max-attributes-per-line": "off",
-    "prettier/prettier": ["error", { "semi": false }]
   }
 }
