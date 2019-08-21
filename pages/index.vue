040404147698498b1078da3e4bcf616f2393ec46
Shota Kato <sheeptech@vivaldi.net>
2019-08-21 11:38:50 +0900

    コード整形？された

--- pages/index.vue
+++ pages/index.vue
@@ -2,25 +2,31 @@
   <section class="container">
     <b-row>
       <b-card v-for="item in data" :key="item" class="text-center">
-        {{ item.dt_txt }}<br>
-        <img :src="'http://openweathermap.org/img/wn/' + item.weather[0].icon + '@2x.png'" width=100 height=100><br>
-        {{ item.weather[0].main }}<br>
-        {{ (item.main.temp - 273).toFixed(1) }}°C<br>
-        {{ item.main.humidity }}%<br>
-        {{ item.wind.speed.toFixed(1) }}m/s<br>
+        {{ item.dt_txt }}<br >
+        <img :src="'http://openweathermap.org/img/wn/' + item.weather[0].icon + '@2x.png'"
+          width="100"
+          height="100"
+        >
         <br>
+        {{ item.weather[0].main }}<br >
+        {{ (item.main.temp - 273).toFixed(1) }}°C<br >
+        {{ item.main.humidity }}%<br >
+        {{ item.wind.speed.toFixed(1) }}m/s<br >
+        <br >
       </b-card>
     </b-row>
   </section>
 </template>
 
 <script>
-
 export default {
   async asyncData({ $axios }) {
-    const res = await $axios.$get("http://api.openweathermap.org/data/2.5/forecast?q=Tokyo&appid=bd1b3bed582f9a23b45d10332927f2fc")
+    const res = await $axios
+      .$get(
+        "http://api.openweathermap.org/data/2.5/forecast?q=Tokyo&appid=bd1b3bed582f9a23b45d10332927f2fc"
+      )
       .catch(err => {
-        console.error('response error', err)
+        console.error("response error", err)
         return false
       })
     return { data: res.list }
